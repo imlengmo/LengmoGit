5c610c22bba95712f10715c2f1be10d2dd635661
zhongam <123456>
2022-01-06 16:19:55 +0800

    微服务生态

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atgugu/cloudalibaba/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.atgugu.cloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zam
+ * @date 2022/1/6 -10:20
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atgugu/cloudalibaba/PaymentMain9002.java
@@ -0,0 +1,18 @@
+package com.atgugu.cloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zam
+ * @date 2022/1/6 -10:28
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+
+}
